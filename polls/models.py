fe65b9dbee1495cc74b34a2cc1093a24e2e7f2a6
an44andedu <an44andedu@gmail.com>
2020-11-13 17:37:43 +0900

    master 2
    https://docs.djangoproject.com/ko/3.1/intro/tutorial04/
    
    까지 보고 코드 따라하기.

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    class Meta:
+        db_table = 'TB_QUESTION'
+        verbose_name = '질문목록'
+        verbose_name_plural = '질문목록'
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = 'TB_QUESTION_CHOICE'
+        verbose_name = '선택목록'
+        verbose_name_plural = '선택목록'
+
+    def __str__(self):
+        return self.choice_text
+
+
