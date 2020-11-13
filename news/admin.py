819059b90a999fb1e4c4094325fbb992889a9a0d
an44andedu <an44andedu@gmail.com>
2020-11-13 15:58:04 +0900

    news/admin.py 어드민 필드테이블 내용 추가
    news 앱 model 한글화 작업
    
    admin.site 헤더, 타이틀, 첫페이지 타이틀 수정함.

--- news/admin.py
+++ news/admin.py
@@ -1,18 +1,5 @@
 from django.contrib import admin
 from .models import Article, Reporter
 
-
-class ArticleAdmin(admin.ModelAdmin):
-    search_fields = ['headline']
-    list_display = ('id', 'headline', 'reporter', 'pub_date')
-    list_display_links = ('id', 'headline')
-
-
-class ReporterAdmin(admin.ModelAdmin):
-    search_fields = ['full_name']
-    list_display = ('id', 'full_name')
-    list_display_links = ('id', 'full_name')
-
-
-admin.site.register(Article, ArticleAdmin)
-admin.site.register(Reporter, ReporterAdmin)
+admin.site.register(Article)
+admin.site.register(Reporter)
--- news/models.py
+++ news/models.py
@@ -2,26 +2,17 @@ from django.db import models
 
 
 class Reporter(models.Model):
-    full_name = models.CharField(max_length=70, verbose_name='작성자명')
+    full_name = models.CharField(max_length=70)
 
     def __str__(self):
         return self.full_name
 
-    class Meta:
-        verbose_name = '작성자목록'
-        verbose_name_plural = '작성자목록'
-
 
 class Article(models.Model):
-    pub_date = models.DateTimeField(verbose_name='작성일시')
-    headline = models.CharField(max_length=200, verbose_name='제목')
-    content = models.TextField(verbose_name='내용')
-    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE, verbose_name='작성자명')
+    pub_date = models.DateTimeField()
+    headline = models.CharField(max_length=200)
+    content = models.TextField()
+    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.headline
-
-    class Meta:
-        verbose_name = '기사목록'
-        verbose_name_plural = '기사목록'
-        ordering = ('-pub_date', '-pk')
