c8664a0f8c40b057ee8ee3ed3a0aba68e4935d7b
an44andedu <an44andedu@gmail.com>
2020-11-16 13:46:47 +0900

    admin 페이지 원복

--- polls/admin.py
+++ polls/admin.py
@@ -1,10 +1,6 @@
-import datetime
-
 from django.contrib import admin
 
 # Register your models here.
-from django.utils import timezone
-
 from polls.models import Question, Choice
 
 
@@ -14,21 +10,12 @@ class ChoiceInline(admin.StackedInline):
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    search_fields = ['question_text']
     list_display = ('id', 'question_text', 'pub_date')
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
-
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
-
-    was_published_recently.admin_order_field = 'pub_date'
-    was_published_recently.boolean = True
-    was_published_recently.short_description = 'Published recently?'
 
 
 class ChoiceAdmin(admin.ModelAdmin):
